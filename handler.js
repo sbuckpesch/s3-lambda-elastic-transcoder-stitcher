5f72f0667a5582a43b02979ba245223945a35aed
Sebastian Buckpesch <s.buckpesch@gmail.com>
2018-05-26 03:04:57 +0200

    Fix async video processing

--- handler.js
+++ handler.js
@@ -11,7 +11,7 @@ const transcoder = new AWS.ElasticTranscoder({
 });
 const s3 = new AWS.S3();
 
-module.exports.stitchVideos = (event, context, callback) => {
+module.exports.stitchVideos = async (event, context, callback) => {
 
     // Read options from the event.
     console.log("Reading options from event:\n", util.inspect(event, {depth: 5}));
@@ -49,7 +49,7 @@ module.exports.stitchVideos = (event, context, callback) => {
         console.log('No folder name specified ' + dir);
         return;
     }
-    if (elasticTranscodeOptions.fileName.includes('output') ) {
+    if (elasticTranscodeOptions.fileName.includes('output')) {
         console.log('The file is already processed. Skip it ' + elasticTranscodeOptions.filePath);
         return;
     }
@@ -65,70 +65,112 @@ module.exports.stitchVideos = (event, context, callback) => {
     const videoFormats = {
         mp4720p: '1351620000001-000010',
         webm720p: '1351620000001-100240',
-    }
+    };
 
-    // Video file input
-    let videoInput = [{
-        Key: elasticTranscodeOptions.filePath,
-        FrameRate: 'auto',
-        Resolution: 'auto',
-        AspectRatio: 'auto',
-        Interlaced: 'auto',
-        Container: 'auto'
-    }];
-
-    // Video outputs
-    const videoOutput = [{
-        Key: elasticTranscodeOptions.filePath + '.output.720p.mp4',
-        //ThumbnailPattern: elasticTranscodeOptions.filePath + '-{count}',
-        PresetId: videoFormats.mp4720p, //Generic 720p
-        Watermarks: [{
-            InputKey: 'watermarks/logo.png',
-            PresetWatermarkId: 'BottomRight'
-        }],
-    }, {
-        Key: elasticTranscodeOptions.filePath + '.output.720p.webm',
-        ThumbnailPattern: '',
-        PresetId:  videoFormats.webm720p, //Webm 720p
-        Watermarks: [{
-            InputKey: 'watermarks/logo.png',
-            PresetWatermarkId: 'BottomRight'
-        }],
-    }];
-
-    // Check if intro.mp4 file is available in the folder
-    console.log(`Check if  ${dir}/intro.mp4 is available in bucket ${elasticTranscodeOptions.bucket}`);
-    s3.getObject({
-        Bucket: elasticTranscodeOptions.bucket,
-        Key: `${dir}/intro.mp4`
-    }, function (err, data) {
-        if (!err) {
-            console.log(`${dir}/intro.mp4 added as intro`);
-            // Add intro to the beginning of the video
-            videoInput.unshift({
-                Key: `${dir}/intro.mp4`,
-                FrameRate: 'auto',
-                Resolution: 'auto',
-                AspectRatio: 'auto',
-                Interlaced: 'auto',
-                Container: 'auto'
-            });
-        }
-    });
-
-    // Check if outro.mp4 is available in the folder
-    console.log("Video input:\n", util.inspect(videoInput, {depth: 5}));
-    console.log("Video out:\n", util.inspect(videoOutput, {depth: 5}));
-
-    transcoder.createJob({
+    // Prepare transcoding params
+    let params = {
         PipelineId: elasticTranscodeOptions.pipelineId,
-        Inputs: videoInput,
-        Outputs: videoOutput,
+        Inputs: [{
+            Key: elasticTranscodeOptions.filePath,
+            FrameRate: 'auto',
+            Resolution: 'auto',
+            AspectRatio: 'auto',
+            Interlaced: 'auto',
+            Container: 'auto'
+        }],
+        Outputs: [
+            {
+                Key: elasticTranscodeOptions.filePath + '.output.720p.mp4',
+                ThumbnailPattern: elasticTranscodeOptions.filePath + '-{count}',
+                PresetId: videoFormats.mp4720p, //Generic 720p
+            },
+            {
+                Key: elasticTranscodeOptions.filePath + '.output.720p.webm',
+                ThumbnailPattern: '',
+                PresetId: videoFormats.webm720p, //Webm 720p
+            }
+        ],
         UserMetadata: {
             date: new Date().toISOString(),
             copyright: 'App-Arena.com'
         }
-    }, function (err, data) {
+    };
+
+    // Check if intro.mp4 file is available in the folder
+    let introAvailable = true;
+    try {
+        await s3.getObject({
+            Bucket: elasticTranscodeOptions.bucket,
+            Key: `${dir}/intro.mp4`
+        }).promise();
+    } catch (err) {
+        introAvailable = false;
+        console.log(`${dir}/intro.mp4 not available`);
+    }
+    if (introAvailable) {
+        // Add intro to the beginning of the video
+        console.log(`${dir}/intro.mp4 added to the beginning`);
+        params.Inputs.unshift({
+            Key: `${dir}/intro.mp4`,
+            FrameRate: 'auto',
+            Resolution: 'auto',
+            AspectRatio: 'auto',
+            Interlaced: 'auto',
+            Container: 'auto'
+        });
+    }
+
+    // Check if outro.mp4 file is available in the folder
+    let outroAvailable = true;
+    try {
+        await s3.getObject({
+            Bucket: elasticTranscodeOptions.bucket,
+            Key: `${dir}/outro.mp4`
+        }).promise();
+    } catch (err) {
+        outroAvailable = false;
+        console.log(`${dir}/outro.mp4 not available`);
+    }
+    if (outroAvailable) {
+        console.log(`${dir}/outro.mp4 added as outro`);
+        // Add outro to the beginning of the video
+        params.Inputs.push({
+            Key: `${dir}/outro.mp4`,
+            FrameRate: 'auto',
+            Resolution: 'auto',
+            AspectRatio: 'auto',
+            Interlaced: 'auto',
+            Container: 'auto'
+        });
+    }
+
+    // Check if watermark.mp4 file is available in the folder
+    let watermarkAvailable = true;
+    try {
+        await s3.getObject({
+            Bucket: elasticTranscodeOptions.bucket,
+            Key: `${dir}/watermark.png`
+        }).promise();
+    } catch (err) {
+        watermarkAvailable = false;
+        console.log(`${dir}/watermark.png not available`);
+    }
+    if (watermarkAvailable) {
+        console.log(`${dir}/watermark.png added as watermark on the bottom right`);
+        // Add watermark to all output
+        let i;
+        for (i = 0; i < params.Outputs.length; i++) {
+            let output = params.Outputs[i];
+            output.Watermarks = [{
+                InputKey: `${dir}/watermark.png`,
+                PresetWatermarkId: 'BottomRight'
+            }];
+            params.Outputs[i] = output;
+        }
+    }
+
+    console.log("Job configuration:\n", util.inspect(params, {depth: 5}));
+    transcoder.createJob(params, function (err, data) {
         if (err) {
             console.log('Something went wrong:', err)
         } else {
@@ -137,109 +179,5 @@ module.exports.stitchVideos = (event, context, callback) => {
         callback(err, data);
     });
 
-    /*
-        const response = {
-            statusCode: 200,
-            body: JSON.stringify({
-                message: 'Go Serverless v1.0! Your function executed successfully!',
-                input: event,
-            }),
-        };
-
-        callback(null, response);*/
-};
-
-/**
- * this function receive the request object informations
- * from S3 and try with Elastic Transcoder to
- * encode with a proper format the video uploaded
- */
-function sendVideoToET(manifest, context) {
-    let key = manifest.media;
-    let user = '';
-    if ((manifest.user).length !== 0) {
-        user = manifest.user + '/';
-    }
-
-    let generate_outputs = function (config) {
-        let out = [];
-        for (let key in config) {
-            let in_ = config[key];
-            let out_ = {
-                Key: in_.key ? in_.key : in_.preset,
-                PresetId: in_.preset,
-                Rotate: 'auto',
-                Watermarks: [
-                    {
-                        InputKey: 'logo.png',
-                        PresetWatermarkId: 'BottomRight'
-                    }
-                ]
-            };
-
-            if (in_.thumbnail) {
-                out_.ThumbnailPattern = 'thumb/thumbnail' + '_{count}';
-            }
-
-            if (in_.segmentduration) {
-                out_.SegmentDuration = in_.segmentduration;
-            }
-
-            out.push(out_);
-        }
-
-        return out;
-    };
-
-    let params = {
-        PipelineId: pipelineId,
-        OutputKeyPrefix: 'video/' + user + manifest.media + '/',
-        Input: {
-            Key: key
-        },
-        Outputs: generate_outputs([
-            {
-                preset: video_360,
-                segmentduration: '60',
-            }, {
-                preset: video_480,
-                thumbnail: true,
-                segmentduration: '60',
-            }/*,
-            {
-                key: 'video.mp4',
-                preset: video_480p_mp4
-            }*/
-        ]),
-        UserMetadata: {
-            date: manifest.date.toString(),
-            copyright: copyright
-        },
-        Playlists: [
-            {
-                Format: 'HLSv3',
-                Name: 'playlist',
-                OutputKeys: [
-                    video_360,
-                    video_480
-                ]
-            }
-        ]
-    };
-
-
-    let job = eltr.createJob(params);
-
-    job.on('error', function (error, response) {
-        console.log('Failed to send new video ' + key + ' to ET');
-        console.log(error);
-        console.log(error.stack);
-
-        context.fail(error);
-    });
-
-    job.on('success', function (response) {
-        context.succeed('Completed, job to ET sent succesfully!');
-    });
-    job.send();
 }
+;
